d2beca4e81bb027b77b34c2214ba0db6a0b9b766
Jami Äijänen <jami.aijanen@hotmail.com>
2021-12-12 19:12:09 +0200

    trying to get heroku app work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import React from 'react';
 import Customers from './components/Customers'
 import Trainings from './components/Trainings';
-import Calendar from './components/Calendar'
+import Calendar from './components/Calendar';
+import Statistics from './components/Statistics';
 import AppBar from '@mui/material/AppBar';
 import Tabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
@@ -21,11 +22,13 @@ function App() {
           <Tab value="one" label="Customers" />
           <Tab value="two" label="Trainings" />
           <Tab value="three" label="Calendar" />
+          <Tab value="four" label="Statistics" />
         </Tabs>
       </AppBar>
       {value === 'one' && <Customers />}
       {value === 'two' && <Trainings />}
       {value === 'three' && <Calendar />}
+      {value === 'four' && <Statistics />}
     </div>
   );
 }
